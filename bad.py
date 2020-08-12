83bb878a94ee2124af20f8dc44bf8d28f25d1734
Zachary Blackwood <zblackwo@gmail.com>
2020-08-12 14:19:50 -0400

    Add examples of bad or unstyled code

---
+++ bad.py
@@ -0,0 +1,9 @@
+a= "way waywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywaywayway too long of a line"
+
+import something_unused
+
+6 = x
+
+y = [1,2, 3,
+        4,
+5]
---
+++ valid_options.py
@@ -0,0 +1,17 @@
+my_list = [
+    1, 2, 3,
+    4, 5, 6,
+    ]
+my_list = [
+    1, 2, 3,
+    4, 5, 6,
+]
+result = some_function_that_takes_arguments(
+    'a', 'b', 'c',
+    'd', 'e', 'f',
+    )
+result = some_function_that_takes_arguments(
+    'a', 'b', 'c',
+    'd', 'e', 'f',
+)
+
